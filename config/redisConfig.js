9941c268345389ff56a8dcbed7033f4b8aebb715
lynx-hrisi <hrishikeshrajbhar118@gmail.com>
2026-02-02 17:19:17 +0530

    Fixed Redis Connection

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -1,6 +1,13 @@
 import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true });
 
 export const redisClient = new Redis({
     host: process.env.REDIS_HOST,
-    maxRetriesPerRequest: null
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    maxRetriesPerRequest: null,
 }); 
+
+redisClient.on('error', err => console.log('Redis Client Error', err));
